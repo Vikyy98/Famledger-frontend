8feb24ec5fdd5272deb6b0037281e06d743f4eab
Vikyy98 <wvignesh0698@gmail.com>
2025-11-21 23:44:54 +0530

    Next config file updated

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   output: "standalone",
+  experimental: {
+    serverActions: {
+      allowedOrigins: ["*"],
+    },
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
